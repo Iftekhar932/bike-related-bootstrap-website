58ef82ccbd89b752b4322b8d372d4119649268e5
Iftekhar Bin Aziz <iftekharaziz932@gmail.com>
2023-10-07 12:22:25 +0600

    database set successfully

--- app.js
+++ app.js
@@ -1,8 +1,53 @@
 require("dotenv").config();
 require("./config/database").connect();
 const express = require("express");
-
+const JWT = require("jsonwebtoken");
 const app = express();
+
+// mongoose schema
+const User = require("./model/user");
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.post("/register", async (req, res) => {
+  try {
+    const { firstName, lastName, password, email } = req.body;
+
+    // checking if all required information are provided
+    if (!(firstName && lastName && password && email)) {
+      return res.send(400).json({ msg: "All information is required" }); // bad request 400
+    }
+
+    // checking if user already exists
+    const oldUser = User.find({ email });
+
+    if (oldUser.email === email) {
+      return res.send(409).json({ msg: "User already exists" }); // conflict with existing resource 409
+    }
+
+    // saving user  info with mongoose schema
+    const user = await User.create({
+      firstName: firstName,
+      lastName: lastName,
+      email: email.toLowerCase(),
+      password: password,
+    });
+
+    // creating token
+    const token = JWT.sign({ email: email }, process.env.TOKEN_KEY, {
+      expiresIn: "2h",
+    });
+
+    // saving user token
+    user.token = token;
+
+    res.status(201).send(user);
+  } catch (error) {
+    console.log(error, "line 13");
+  }
+});
+app.get("/login", (req, res) => {});
 
 app.use(express.json()); // The express. json() function is a middleware function used in Express. js applications to parse incoming JSON data from HTTP requests
 
---
+++ model/user.js
@@ -0,0 +1,10 @@
+const mongoose = require("mongoose");
+
+const userSchema = new mongoose.Schema({
+  firstName: { type: "string", required: true },
+  lastName: { type: "string", required: true },
+  email: { type: "string", required: true },
+  password: { type: "string", required: true },
+});
+
+module.exports = mongoose.model("user", userSchema);
