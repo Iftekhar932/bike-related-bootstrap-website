c9b0bd8e2c0151ddcaec16018b373476a01a7075
Iftekhar Bin Aziz <iftekharaziz932@gmail.com>
2023-10-18 11:47:59 +0600

    fethcing twice issue solved

--- scripts/script.js
+++ scripts/script.js
@@ -1,14 +1,15 @@
+const bikesContainerBoxID = document.getElementById("bikesContainerBox");
 const firstNameBox = document.getElementById("exampleInputFirstName1");
 const lastNameBox = document.getElementById("exampleInputLastName1");
 const passwordBox = document.getElementById("exampleInputPassword1");
 const emailBox = document.getElementById("exampleInputEmail1");
 const form = document.getElementById("userInfoForm");
-const searchBox = document.querySelector('[aria-label="Search"]'); // in navbar
-const bikeSearchForm = document.getElementById("bikeSearchFormID");
-const bikeSearchButton = document.getElementById("bikeSearchButtonID");
+const searchBox = document.querySelector('[aria-label="Search"]'); // in "bikeDisplay.html"
+const bikeSearchForm = document.getElementById("bikeSearchFormID"); // in "bikeDisplay.html"
+const bikeSearchButton = document.getElementById("bikeSearchButtonID"); // in "bikeDisplay.html"
 
-function sendInfo() {
-  fetch("http://localhost:4001/register", {
+async function sendInfo() {
+  await fetch("http://localhost:4001/register", {
     method: "POST",
     headers: {
       "content-type": "application/json",
@@ -19,17 +20,13 @@ function sendInfo() {
       email: emailBox.value.toLowerCase(),
       password: passwordBox.value,
     }),
-  })
-    .then((res) => res.json())
-    .then((d) => console.log(d))
-    .catch((err) => console.log(err));
+  }).then((res) => res.json());
 }
 
 async function bikeSearcher() {
-  await fetch(`../data/bikeData.json`)
+  await fetch("../data/bikeData.json")
     .then((res) => res.json())
     .then((data) => {
-      console.log(data.motorbikes);
       const searchedFor = data.motorbikes.filter((bike) => {
         return (
           bike.brand.toLowerCase().includes(searchBox.value.toLowerCase()) ||
@@ -39,23 +36,56 @@ async function bikeSearcher() {
           bike.engine.toLowerCase().includes(searchBox.value.toLowerCase())
         );
       });
-      console.log(searchedFor);
+      if (searchedFor) return displayIndividualBike(searchedFor);
     });
 }
 
 searchBox.addEventListener("keydown", (e) => {
-  if (e.key === "Enter") return bikeSearcher();
+  if (e.key === "Enter") {
+    withOrWithout(false);
+    return bikeSearcher();
+  }
 });
 
-bikeSearchForm.addEventListener("submit", function (e) {
-  e.preventDefault();
-  return bikeSearcher();
-});
+function withOrWithout(flag) {
+  if (flag === false) return;
+  return function (e) {
+    e.preventDefault();
+    if (flag) return;
+    console.log("eta");
+    return bikeSearcher();
+  };
+}
 
-// bikeSearchButton.addEventListener("click", async function (e) {
-//   e.preventDefault();
-//   console.log(e.target.value);
-//   /*   fetch(`../data/bikeData.json/${searchText}`)
-//     .then((res) => res.json())
-//     .then((data) => console.log(data.motorbikes)); */
-// });
+bikeSearchForm.addEventListener("submit", withOrWithout());
+
+function displayIndividualBike(bikeInfo) {
+  let bikeInfoToDisplay = bikeInfo;
+
+  bikeInfoToDisplay.map((singleBikeInfo) => {
+    const { id, brand, model, type, engine, price, color, description, image } =
+      singleBikeInfo;
+
+    const singleBikeBox = `
+              <img
+                class="card-img-top"
+                src="${image}"
+                alt="${brand}"
+                height="auto" width="auto"
+              />
+              <div class="card-body">
+                <h5 class="card-title">Brand: ${brand}</h5>
+                <h6 class="card-title">Color: ${color}, Model:${model} with Engine: ${engine}</h6>
+                <h6 class="card-title">Type: ${type}</h6>
+                <p class="card-text">
+               ${description}
+                </p>
+                <span href="#" class="btn btn-primary">$ ${price}</span>
+                </div>`;
+    //
+    const boxToAppend = document.createElement("div");
+    boxToAppend.setAttribute("class", "card p-2 shadow-sm col-3");
+    boxToAppend.innerHTML = singleBikeBox;
+    bikesContainerBoxID.append(boxToAppend);
+  });
+}
